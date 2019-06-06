89d3da57d4b3083d181e97330b1f5f794c3f83e2
breezysafu <breezy.safu@gmail.com>
2019-06-06 17:44:30 +0545

    see trafficmanagement.py and try to add boxes on cntours

--- TrafficManagement.py
+++ TrafficManagement.py
@@ -1,26 +1,36 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("E:\\Projects\\Python\\OpenCV\\traffic.png")
+cap = cv2.VideoCapture(0)
+while (1):
+    ret, frame = cap.read()
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    threshold_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 
-threshold_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+    edges = cv2.Canny(gray_image,100,200)
 
-edges = cv2.Canny(gray_image,100,200)
+    kernel = np.ones((5,5),np.uint8)
+    erosion = cv2.dilate(edges,kernel,iterations=1)
 
-kernel = np.ones((5,5),np.uint8)
-erosion = cv2.dilate(edges,kernel,iterations=1)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
+    img = cv2.drawContours(frame, contours, -1, (255,255,255), 3)
 
-im2, contours, hierarchy = cv2.findContours(erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-box = cv2.drawContours(image, contours, -1, (0,255,0), 3)
+#    cnt = contours[0]
+ #   x, y, w, h = cv2.boundingRect(cnt)
+ #   box=cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-cv2.imshow("Real Image", image)
-cv2.imshow("gray image",gray_image)
-cv2.imshow("Gray image",threshold_image)
-cv2.imshow("edges image",edges)
-cv2.imshow("eroded image",erosion)
-cv2.imshow("eroded image",box)
+    cv2.imshow("Real Image", frame)
+    cv2.imshow("gray image",gray_image)
+    cv2.imshow("threshold image",threshold_image)
+    cv2.imshow("canny",edges)
+    cv2.imshow("eroded image",erosion)
+    cv2.imshow("contours",img)
+   # cv2.imshow("rectangle box", box)
 
-cv2.waitKey(0)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+       break
+
+cap.release()
 cv2.destroyAllWindows()
