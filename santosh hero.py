4f0587e196c07f4e0ef62d47c3c3f3bee32f5f80
Santosh-Devkota <devkota549@gmail.com>
2019-06-06 13:24:31 +0545

    This is my first commit GUYSgit add .

---
+++ santosh hero.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+
+# capture frames from a camera
+cap = cv2.VideoCapture(0)
+#cv2.waitKey(500)
+# loop runs if capturing has been initialized
+while (1):
+    ret, frame = cap.read()
+    edges = cv2.Canny(frame,100,200)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
+    img = cv2.drawContours(frame, contours, -1, (0,0,255), 3)
+    
+    #cv2.imshow("hellboy",edges)
+    cv2.imshow("hello",frame)
+
+    cv2.imshow("hellboy",img)
+    # Wait for Esc key to stop
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+    
+# Close the window
+cap.release()
+# De-allocate any associated memory usage
+cv2.destroyAllWindows()
