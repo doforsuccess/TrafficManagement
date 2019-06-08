d3591f64519e0cc2e1c7b57068f4e734b4b0e57d
breezysafu <breezy.safu@gmail.com>
2019-06-08 22:02:01 +0545

    video successfylly divided along with countours

--- VehicleDetection.py
+++ VehicleDetection.py
@@ -1,20 +1,32 @@
 import cv2
 
 cap = cv2.VideoCapture('video.avi')
-car_cascade = cv2.CascadeClassifier('cars.xml')
+car_cascade = cv2.CascadeClassifier('xmlFiles/cars.xml')
 
 while True:
-    ret, frames = cap.read()
+    ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cars = car_cascade.detectMultiScale(gray, 1.1, 1)
 
     # To draw a rectangle in each cars
     for (x, y, w, h) in cars:
-        cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    cv2.imshow('video2', frames)
+    y = 0
+    x = 0
+    h = 100
+    w = 200
+    # crops the live image from 0 px to 200px along x and from 0px to 100px along y
+    crop1 = frame[y:y + h, x:x + w]
+
+    # crops the live image from 200 px to 400px along x and from 100px to 300px along y
+    crop2 = frame[y + h:300, x + w:400]
+
+    cv2.imshow('first half', crop1)
+    cv2.imshow('second half', crop2)
+
 
     # Wait for Esc key to stop
     if cv2.waitKey(33) == 27:
